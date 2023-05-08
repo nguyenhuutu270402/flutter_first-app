c9c2965fe7fc8baa442e40d42e642c53bdff8b59
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-05-08 10:49:34 +0700

    update 20

---
+++ lib/test/load_more_list_view.dart
@@ -0,0 +1,70 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class LoadMoreListView extends StatefulWidget {
+  const LoadMoreListView({super.key});
+
+  @override
+  State<LoadMoreListView> createState() => _LoadMoreListViewState();
+}
+
+class _LoadMoreListViewState extends State<LoadMoreListView> {
+  List apiList = List.generate(78, (index) => "Item: ${index + 1}");
+  // List myList = [];
+  ScrollController _scrollController = ScrollController();
+  int _currentMax = 10;
+  ValueNotifier<List> myList = ValueNotifier([]);
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    myList.value = List.generate(10, (index) => "Item: ${index + 1}");
+    _scrollController.addListener(() {
+      if (_scrollController.position.pixels ==
+          _scrollController.position.maxScrollExtent) {
+        _getMoreData();
+      }
+    });
+  }
+
+  void _getMoreData() {
+    for (int i = _currentMax; i < _currentMax + 10; i++) {
+      if (i < apiList.length) {
+        myList.value.add("Item:a ${i + 1}");
+      }
+    }
+    _currentMax = _currentMax + 10;
+    if (_currentMax > apiList.length) {
+      _currentMax = apiList.length;
+    }
+    myList.notifyListeners();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Load More"),
+      ),
+      body: ValueListenableBuilder(
+        valueListenable: myList,
+        builder: (context, value, child) {
+          return ListView.builder(
+              controller: _scrollController,
+              itemCount: myList.value.length + 1,
+              itemExtent: 80,
+              itemBuilder: (context, index) {
+                if (index == myList.value.length && index < apiList.length) {
+                  return CupertinoActivityIndicator();
+                }
+                if (index < apiList.length) {
+                  return Text("${myList.value[index]}");
+                } else {
+                  return Text("Hết rồi!!!!");
+                }
+              });
+        },
+      ),
+    );
+  }
+}

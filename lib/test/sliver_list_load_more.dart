20d02bc2e13b61fedb03861549f964eb3e4043b1
Nguyễn Hữu Tứ <95615938+nguyenhuutu270402@users.noreply.github.com>
2023-05-11 11:41:01 +0700

    update 30

--- lib/test/sliver_list_load_more.dart
+++ lib/test/sliver_list_load_more.dart
@@ -11,7 +11,7 @@ class SliverListLoadMore extends StatefulWidget {
 class _SliverListLoadMoreState extends State<SliverListLoadMore> {
   final ScrollController _scrollController = ScrollController();
   ValueNotifier<List<String>> items = ValueNotifier([]);
-
+  // bool _isRefreshing = false;
   bool _isLoading = false;
 
   @override
@@ -49,37 +49,60 @@ class _SliverListLoadMoreState extends State<SliverListLoadMore> {
     items.notifyListeners();
   }
 
+  Future<void> _refreshData() async {
+    // Simulate loading data asynchronously
+    // await Future.delayed(Duration(seconds: 2));
+
+    // // Complete refreshing
+    // setState(() {
+    //   _isRefreshing = false;
+    // });
+    items.value.clear();
+    _fisrtData();
+    items.notifyListeners();
+    print("remake");
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        controller: _scrollController,
-        slivers: [
-          SliverAppBar(
-            title: Text('SliverList Load More'),
-            floating: true,
-            snap: true,
-          ),
-          ValueListenableBuilder(
-            valueListenable: items,
-            builder: (context, value, child) {
-              return SliverList(
-                delegate: SliverChildBuilderDelegate(
-                  (context, index) {
-                    if (index < items.value.length) {
-                      return ListTile(
-                        title: Text(items.value[index]),
-                      );
-                    } else {
-                      return _buildLoader();
-                    }
-                  },
-                  childCount: items.value.length + 1,
-                ),
-              );
-            },
-          )
-        ],
+      body: RefreshIndicator(
+        onRefresh: _refreshData,
+        displacement:
+            10.0, // Khoảng cách di chuyển của indicator khi được kéo xuống
+        color: Colors.red, // Màu sắc của indicator
+        backgroundColor: Colors.amber, // Màu nền của indicator
+        strokeWidth: 2.0,
+
+        child: CustomScrollView(
+          controller: _scrollController,
+          slivers: [
+            SliverAppBar(
+              title: Text('SliverList Load More'),
+              floating: true,
+              snap: true,
+            ),
+            ValueListenableBuilder(
+              valueListenable: items,
+              builder: (context, value, child) {
+                return SliverList(
+                  delegate: SliverChildBuilderDelegate(
+                    (context, index) {
+                      if (index < items.value.length) {
+                        return ListTile(
+                          title: Text(items.value[index]),
+                        );
+                      } else {
+                        return _buildLoader();
+                      }
+                    },
+                    childCount: items.value.length + 1,
+                  ),
+                );
+              },
+            )
+          ],
+        ),
       ),
     );
   }
